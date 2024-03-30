c27b2798fe290d89b82e386361ffa87ab4894ebe
Baladurage Kavinda <63805835+BK-KAVIYA@users.noreply.github.com>
2024-03-30 10:07:05 +0530

    navigation bar completed

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 import Logo from '../../../public/images/Logo.png'
 import './Navbar.css'
 import { Link, NavLink } from 'react-router-dom';
+import {FaBarsStaggered, FaXmark} from 'react-icons/fa6'
 
 function Navbar() {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const toggleMenu = () => {
+    const handleMenuToggler = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
@@ -23,28 +24,46 @@ function Navbar() {
             <span>NiceJob</span>
         </a>
             {/* <!-- nav items for larger device --> */}
-            <ul className='hidden md:flex gap-12 '>
-                {
-                    navitems.map(({path,title})=>(
-                        <li key={path} className='text-base text-primary'>
-                            <NavLink
-                                to={path}
-                                className={({ isActive}) =>
-                                    isActive ? "active" : ""
-                                }
-                            >
-                                {title}
-                            </NavLink>
-                        </li>
-                    ))
-                }
+            <ul className='hidden md:flex gap-12'>
+                {navitems.map(({ path, title }) => (
+                    <li key={path} className='w-50 h-50 text-base text-primary hover-effect font-bold first:text-blue'>
+                        <NavLink
+                            to={path}
+                            activeClassName="active"
+                        >
+                            {title}
+                        </NavLink>
+                    </li>
+                ))}
             </ul>
             {/* singup and login button */}
             <div className='text-base text-primary font-medium space-x-5 hidden lg:block'>
-                <Link to='/login' className='py-2 px-5 border rounded'>Login</Link>
-                <Link to='/signup' className='py-2 px-5 border rounded bg-blue text-white'>Sign Up</Link>
+                <Link to='/login' className='py-2 px-5 border rounded hover:bg-gray-300'>Login</Link>
+                <Link to='/signup' className='py-2 px-5 border rounded bg-blue text-white  hover:bg-cyan-950'>Sign Up</Link>
+            </div>
+            <div className='md:hidden block'>
+                <button onClick={handleMenuToggler}>
+                    {
+                        isMenuOpen ? <FaXmark className='w-5 h-5 text-primary'/> : <FaBarsStaggered  className='w-5 h-5 text-primary'/>
+                    }
+                </button>
             </div>
         </nav>
+        <div className={`px-4 bg-black py-5 rounded-sm ${isMenuOpen ? "transition-all duration-1000 opacity-100" : "hidden"}`}>
+    <ul>
+        {navitems.map(({ path, title }) => (
+            <li key={path} className='text-base text-white first:text-blue py-1'>
+                <NavLink
+                    to={path}
+                    activeClassName="active"
+                >
+                    {title}
+                </NavLink>
+            </li>
+        ))}
+        <li className='text-white py-1'><Link to='/login' >Login</Link></li>
+    </ul>
+</div>
     </header>
   )
 }
