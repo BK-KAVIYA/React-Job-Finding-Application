d528bb33c5a801e3b0cb5b4f450e04f8a7edf84b
Baladurage Kavinda <63805835+BK-KAVIYA@users.noreply.github.com>
2024-04-05 14:41:06 +0530

    Add UIs

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ function Banner({ query, handleInputChange }) {
   return (
     <div className="max-w-screen-2xl container mx-auto xl:px-24 px-4 md:py-20 py-14">
       <h1 className="text-5xl font-bold text-primary mb-3">
-        Find your <span className="text-blue">new job</span> today
+        Find Your <span className="text-blue">New Job</span> Today
       </h1>
       <p className="text-lg text-black/70 mb-8">
         Thousands of jobs in the computer, engineering and technology sectors
